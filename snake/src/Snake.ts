561be6563205cf75643a1ef72a1505d7396dd8c5
Emilien <emilien1005@hotmail.fr>
2020-12-15 22:37:10 +0100

    :sparkles: Add move like the original snake game

--- snake/src/Snake.ts
+++ snake/src/Snake.ts
@@ -1,40 +1,44 @@
 import Game from "./Game";
 import Grid from "./Grid";
 
+type SnakeDirection = 'left' | 'right' | 'up' | 'down';
+
 export default class Snake extends Game {
   private grid: Grid;
-  x: number;
-  y: number;
+  private speed: number = 50; 
+  private direction: SnakeDirection = 'right';
 
   constructor() {
     super();
-    this.x = 0;
-    this.y = 0;
     this.grid = new Grid({ width: 50, height: 20, border: true });
     this.grid.addItem({ name: 'snake', value: '-', x: 0, y: 0, follow: true });
     this.grid.addItem({ name: 'start', value: '*', x: 25, y: 10 });
+    setInterval(this.move.bind(this), this.speed);
   }
 
   protected onFrame() {
     this.writeGrid(this.grid);
   }
 
-  protected onKeypress(str: any, key: any) {
-    switch (key.name) {
+  private move() {
+    switch (this.direction) {
       case 'right':
         return this.grid.moveItem('snake', 1, 0);
-        
+      
       case 'left':
         return this.grid.moveItem('snake', -1, 0);
-      
-      case 'down':
-        return this.grid.moveItem('snake', 0, 1);
-      
+        
       case 'up':
         return this.grid.moveItem('snake', 0, -1);
-      
-        default:
-        break;
+
+      case 'down':
+        return this.grid.moveItem('snake', 0, 1);
+    }
+  }
+
+  protected onKeypress(str: any, key: any) {
+    if (['right', 'left', 'up', 'down'].includes(key.name)) {
+      this.direction = key.name;
     }
   }
 }
