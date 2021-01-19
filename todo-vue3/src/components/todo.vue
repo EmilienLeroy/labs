f797039295d1227c6b4fad415a2114b6c0c1e737
Emilien <emilien1005@hotmail.fr>
2021-01-19 22:34:24 +0100

    :sparkles: feature to add todo

---
+++ todo-vue3/src/components/todo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ name }}
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    id: null,
+    name: '',
+    checked: false,
+  }
+}
+</script>
--- todo-vue3/src/components/todos.vue
+++ todo-vue3/src/components/todos.vue
@@ -1,12 +1,43 @@
 <template>
   <div>
-    TODO
+    <input type="text" v-model="name">
+    <input type="submit" @click.prevent="addTodo">
+    <todo 
+      v-for="todo in todos" 
+      :key="todo.id" 
+      :name="todo.name" 
+    />
   </div>
 </template>
 
 
 <script>
+import { ref } from 'vue';
+import todo from './todo.vue';
+
 export default {
-  
+  components:  {
+    todo,
+  },
+  setup() {
+    let id = ref(0);
+    const name = ref('');
+    const todos = ref([]);
+
+    const addTodo = () => {
+      id.value ++;
+      todos.value.push({
+        id: id.value,
+        name: name.value,
+        checked: false,
+      })
+    }
+
+    return {
+      name,
+      todos,
+      addTodo
+    }
+  }
 }
 </script>
