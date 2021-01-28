581e8f8030f8ded431cc19475966c815c59fe00e
Emilien <emilien1005@hotmail.fr>
2021-01-28 21:12:28 +0100

    :sparkles: Add swagger docs

--- todo-tsed/src/Server.ts
+++ todo-tsed/src/Server.ts
@@ -3,6 +3,7 @@ import {Configuration, Inject} from "@tsed/di";
 import {$log, BeforeRoutesInit, PlatformApplication} from "@tsed/common";
 import "@tsed/platform-express"; // /!\ keep this import
 import "@tsed/mongoose"; 
+import "@tsed/swagger";
 import bodyParser from "body-parser";
 import compress from "compression";
 import cookieParser from "cookie-parser";
@@ -51,6 +52,16 @@ if (isProduction) {
       `${rootDir}/controllers/**/*.ts`
     ]
   },
+  swagger: [
+    {
+      path: "/v2/docs",
+      specVersion: "2.0",
+    },
+    {
+      path: "/v3/docs",
+      specVersion: "3.0.1"
+    }
+  ],
   exclude: [
     "**/*.spec.ts"
   ]
