b9c2ee8ae744e9b93bfc96ae7d8a5f8e118bb35a
Emilien <emilien1005@hotmail.fr>
2021-04-06 21:53:18 +0200

    :heavy_plus_sign: Add oak module

--- todo/server-deno/app.ts
+++ todo/server-deno/app.ts
@@ -1,1 +1,9 @@
-console.log('ok')
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+
+app.use(({ response }) => {
+  response.body = 'hi'
+});
+
+app.listen({ port: 8083 });
--- todo/server-deno/scripts.config.ts
+++ todo/server-deno/scripts.config.ts
@@ -4,7 +4,7 @@ import { DenonConfig } from "https://deno.land/x/denon@2.4.7/mod.ts";
 const config: DenonConfig = {
   scripts: {
     start: {
-      cmd: "deno run app.ts",
+      cmd: "deno run --allow-net app.ts",
       desc: "run my app.ts file",
     },
   },
