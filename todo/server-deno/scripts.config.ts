8ae9b8167f096776db12529705f79d02cf314f7c
Emilien <emilien1005@hotmail.fr>
2021-04-06 19:51:32 +0200

    :sparkles: Adding deno todo app

---
+++ todo/server-deno/scripts.config.ts
@@ -0,0 +1,13 @@
+
+import { DenonConfig } from "https://deno.land/x/denon@2.4.7/mod.ts";
+
+const config: DenonConfig = {
+  scripts: {
+    start: {
+      cmd: "deno run app.ts",
+      desc: "run my app.ts file",
+    },
+  },
+};
+
+export default config;
