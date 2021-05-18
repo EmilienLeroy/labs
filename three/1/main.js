59df400e9445bd27f543f30f5309343582655d17
Emilien Leroy <emilien1005@hotmail.fr>
2021-05-18 21:55:57 +0000

    ✨ (three): Add a beautiful text

--- three/1/main.js
+++ three/1/main.js
@@ -1,6 +1,7 @@
 import './style.css'
-import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh, CircleBufferGeometry, CircleGeometry } from 'three';
+import { FontLoader,Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh, CircleBufferGeometry, CircleGeometry, TextGeometry } from 'three';
 
+const font = await new FontLoader().loadAsync('https://raw.githubusercontent.com/mrdoob/three.js/master/examples/fonts/gentilis_regular.typeface.json');
 const body = document.body;
 const width = window.innerWidth;
 const height = window.innerHeight;
@@ -10,15 +11,19 @@ const scene2 = new Scene();
 const camera = new PerspectiveCamera(75, ratio, 0.1, 1000);
 const renderer = new WebGLRenderer();
 const material = new MeshBasicMaterial({ color: 'red' }) 
-const cube = new Mesh(new BoxGeometry(), material);
+const cube = new Mesh(new BoxGeometry(25, 25, 25), material);
 const circle = new Mesh(new CircleGeometry(1, 100), material);
+const text = new TextGeometry('Hello le world', { font, size: 15, height: 7 });
+
 
 cube.name = 'main';
 circle.name = 'main';
 
-camera.position.z = 5;
+cube.position.set(-20, 0, 0)
+camera.position.set(50, 10, 100)
 
 scene.add(cube);
+scene.add(new Mesh(text, new MeshBasicMaterial({ color: 'blue' })));
 scene2.add(circle);
 
 renderer.setSize(width, height);
