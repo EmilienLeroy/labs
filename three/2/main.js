7e3d0ed5863581feccc7f7e27990501171c07d14
Emilien Leroy <emilien1005@hotmail.fr>
2021-05-25 21:08:24 +0000

    ✨ (three): Create clock

---
+++ three/2/main.js
@@ -0,0 +1,42 @@
+import './style.css'
+import { Scene, PerspectiveCamera, WebGLRenderer, LineBasicMaterial, BufferGeometry, Vector3, Vector4, Line, CylinderGeometry, MeshBasicMaterial, Mesh } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const body = document.body;
+const width = window.innerWidth;
+const height = window.innerHeight;
+const ratio =  width / height;
+const material = new LineBasicMaterial({ color: 'red', linewidth: 100 });
+const mesh = new MeshBasicMaterial({ color: 'white' });
+const scene = new Scene();
+const camera = new PerspectiveCamera(75, ratio, 0.1, 1000);
+const renderer = new WebGLRenderer();
+const geoCylinder = new CylinderGeometry(60, 60, 1, 65);
+const cylinder = new Mesh(geoCylinder, mesh);
+const geometry = new BufferGeometry().setFromPoints([
+  new Vector3(0, 0, 0),
+  new Vector3(50, 0, 0),
+  new Vector3(0, 0, 0),
+]);
+
+const line = new Line(geometry, material);
+const control = new OrbitControls(camera, renderer.domElement);
+
+line.position.set(0, 0, 1);
+cylinder.rotateX(1.57);
+scene.add(line);
+scene.add(cylinder);
+camera.position.set(0, 0, 100);
+renderer.setSize(width, height);
+body.append(renderer.domElement);
+
+main(renderer, scene, camera, line);
+
+setInterval(() => {
+  line.rotateZ(-0.1);
+}, 1000)
+
+function main(renderer, scene, camera) {
+  requestAnimationFrame(() => main(renderer, scene, camera));
+  renderer.render(scene, camera);
+}
